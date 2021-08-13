c52e57003577a74d5264f6c79f1ba5184f586981
lucas <lucasmacuna@gmail.com>
2021-08-13 01:36:15 -0300

    Modification 16

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let start;
+let cont=1;
 const num7 = (id)=>{let numero =document.querySelectorAll("button")
      
     for(let i =0;i<numero.length;i++){
-            if(id===numero[i].id&&start===true){
+            if(id===numero[i].id&&start===true&&cont<13){
                 n7=document.querySelector("#resultado").innerText+=id;
+                 cont++;
             }
     }
 }
@@ -12,8 +14,9 @@ const num7 = (id)=>{let numero =document.querySelectorAll("button")
 const num8 = (id)=>{let numero =document.querySelectorAll("button")
     
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n8=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 
@@ -21,66 +24,75 @@ for(let i =0;i<numero.length;i++){
 const num9 = (id)=>{let numero =document.querySelectorAll("button")
 
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n9=document.querySelector("#resultado").innerText+=id;
+        cont++; 
     }
 }
 }
 const num4 = (id)=>{let numero =document.querySelectorAll("button")
 
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n4=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num5 = (id)=>{let numero =document.querySelectorAll("button")
 
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n5=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num6 = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n6=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num1 = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n1=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num2 = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n2=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num3 = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n3=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num0 = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n0=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
 const num = (id)=>{let numero =document.querySelectorAll("button")
 for(let i =0;i<numero.length;i++){
-    if(id===numero[i].id&&start===true){
+    if(id===numero[i].id&&start===true&&cont<13){
         n=document.querySelector("#resultado").innerText+=id;
+        cont++;
     }
 }
 }
@@ -90,8 +102,9 @@ for(let i =0;i<numero.length;i++){
 const simbolo=(id)=>{let op= document.querySelectorAll("button")
 
 for(let i=0;i<op.length;i++){
-    if(id===op[i].id&&start===true&&(id==="R"||id==="n"||id==="x"||id==="+"||id==="-"||id==="/")){
+    if(id===op[i].id&&start===true&&cont<=1&&(id==="R"||id==="n"||id==="x"||id==="+"||id==="-"||id==="/")){
        simb= document.querySelector("#resultado").innerText+=id;
+       cont++;
     }
 }
 
@@ -105,27 +118,38 @@ const operacion=()=>{
     
     
         for(let i =0;i<datos.length;i++){
-            
-            if(datos[i]==="+"||datos[i]==="-"||datos[i]==="x"||datos[i]==="/"||datos[i]==='R'||datos[i]==='n'){
+            if(datos[i]==="-"||datos[i]==="+"||datos[i]==="x"||datos[i]==="/"||datos[i]==='R'||datos[i]==='n'){
+                  
+
                 break;
             }
-                n1+=datos[i];   
-                   
+            n1+=datos[i];
+          
+              
+            
+            
+           
         }
         for(let j =n1.length+1;j<datos.length;j++){
             
-          
-            if(datos[j]!=="+"||datos[j]!=="-"||datos[j]!=="x"||datos[j]!=="/"||datos[i]!=="R"||datos[i]!=="n"){
-                
-                n2+=datos[j];
-            }
             
+            if(datos[j]!=="-"&&datos[j]!=="+"&&datos[j]!=="x"&&datos[j]!=="/"&&datos[j]!=="R"&&datos[j]!=="n"){
+                n2+=datos[j];
+                
+            }
+             
 
         }
-        
-           n1=parseFloat(n1);
+        n2=parseFloat(n2)
+           
+           
+           
+           parseFloat(n1)
+           console.log(n1)
+           n1=parseFloat(n1)
            n2=parseFloat(n2); 
-
+           console.log(n1)
+           console.log(n2); 
        
                     
     
@@ -135,32 +159,37 @@ const operacion=()=>{
                 
                 
                  let r=parseFloat(n1)+parseFloat(n2);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2) ; 
               }else if(datos[i]==="-"){
                   let r =parseFloat(n1)-parseFloat(n2);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2); 
+                   
               }else if(datos[i]==="x"){
                   let r =parseFloat(n1)*parseFloat(n2);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2); 
+                   
               }else if(datos[i]==="/"){
                   let r =parseFloat(n1)/parseFloat(n2);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2); 
+                   
               }else if(datos[i]==='R'){
                   
                   let r=Math.sqrt(n1);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2); 
+                   
               }else if(datos[i]==='n'){
                   let r =Math.pow(n1,n2||2);
-                  return document.querySelector("#resultado").innerText=r.toFixed(2);
+                  return document.querySelector("#resultado").innerText=r.toFixed(2); 
+                   
               }
-              console.log(n1)
-              console.log(n2); 
-             
-        }             
+              
+              cont=0;
+        } 
+                  
       
 }
  
-const borrar =()=>document.querySelector("#resultado").innerText="";n1=0;n2=0;
+const borrar =()=>{document.querySelector("#resultado").innerText="";n1=0;n2=0;cont=0;}
 
 
    
@@ -208,3 +237,4 @@ const restablecer=()=>{
         document.querySelector("#resultado").innerText="Memoria vacia";
     }
 }
+
